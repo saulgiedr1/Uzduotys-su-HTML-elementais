62564114b9c6332c9f650fcae1ec7eb9ad060724
saulgiedr1 <saulgiedr@gmail.com>
2022-06-20 18:43:29 +0300

    baigta pirma uzduotis

--- U1-staciakampio-perimetras/script.js
+++ U1-staciakampio-perimetras/script.js
@@ -1,3 +1,29 @@
 window.onload=function(){
     
+    let ilgioIvedimoLaukelis = document.getElementById("ilgis");
+    console.log(ilgioIvedimoLaukelis);
+
+    let plocioIvedimoLaukelis = document.getElementById("plotis");
+
+    let mygtukas = document.getElementById("skaiciavimoMygtukas");
+    console.log(mygtukas);
+
+    let pastraipa = document.getElementById("rezultatoPastraipa");
+
+    mygtukas.onclick = function() {
+        let ilgis = ilgioIvedimoLaukelis.valueAsNumber;
+        console.log(ilgis);
+
+        let plotis = plocioIvedimoLaukelis.valueAsNumber;
+        console.log(plotis);
+
+        console.log(perimetras(ilgis, plotis));
+
+        pastraipa.innerHTML = "Perimetras yra: " + perimetras(ilgis, plotis);
+    }
+}
+
+
+function perimetras(a, b) {
+    return 2*a + 2*b;
 }
